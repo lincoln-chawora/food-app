da2dec0327254b55399e84560dc350a051710a2d
Lincoln Chawora <Lincoln_1996@hotmail.com>
2024-01-23 12:21:36 +0000

    remove unwanted files

--- App.js
+++ App.js
@@ -1,78 +1,20 @@
 import React, {useState} from 'react';
-import {Button, FlatList, StyleSheet, View} from 'react-native';
+import {Text, FlatList, StyleSheet, View} from 'react-native';
 import { StatusBar } from "expo-status-bar";
-import GoalItem from "./components/GoalItem";
-import GoalInput from "./components/GoalInput";
 
 export default function App() {
-  const [modalIsVisible, setModalIsVisible] = useState(false)
-  const [lifeGoals, setLifeGoals] = useState([]);
-
-  function startAddGoalHandler() {
-    setModalIsVisible(true);
-  }
-
-  function endAddGoalHandler() {
-    setModalIsVisible(false);
-  }
-  function addGoalHandler(enteredGoalText) {
-    setLifeGoals((currentLifeGoals) => [...currentLifeGoals,
-      {text: enteredGoalText, id: Math.random().toString()}
-    ]);
-    endAddGoalHandler();
-  }
-
-  function deleteGoalHandler(id) {
-    setLifeGoals(currentLifeGoals => {
-      return currentLifeGoals.filter((goal) => goal.id !== id);
-    });
-  }
-
   return (
-    <>
-      <StatusBar style='light'></StatusBar>
-    <View style={styles.appContainer}>
-
-      <GoalInput visible={modalIsVisible}
-                 onAddGoal={addGoalHandler}
-                 onCancel={endAddGoalHandler} />
-
-      <View style={styles.goalsContainer}>
-        <FlatList data={lifeGoals} renderItem={itemData => {
-          return <GoalItem
-              id={itemData.item.id}
-              text={itemData.item.text}
-              onDeleteItem={deleteGoalHandler} />
-        }}
-        keyExtractor={(item, index) => {
-          return item.id;
-        }}
-        >
-        </FlatList>
+      <View style={styles.container}>
+        <Text>Hello World!!!!</Text>
+        <StatusBar style="auto" />
       </View>
-
-      <Button title="Add New Goal" color="#a065ec" onPress={startAddGoalHandler} />
-    </View>
-    </>
   );
 }
 
 const styles = StyleSheet.create({
-  appContainer: {
+  container: {
     flex: 1,
     paddingVertical: 50,
     paddingHorizontal: 16
   },
-  goalsContainer: {
-    flex: 5
-  },
-  goalItem: {
-    margin: 8,
-    padding: 8,
-    borderRadius: 6,
-    backgroundColor: '#5e0acc',
-  },
-  goalText: {
-    color: 'white'
-  }
 });
